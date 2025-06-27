4b9511442ef03e50951beb5a0530f4d59be995f3
Filippo Vecchiato <filippo@parity.io>
2025-06-27 16:08:50 +0200

    cleanup config file read for logging

--- packages/utils/src/logger.ts
+++ packages/utils/src/logger.ts
@@ -25,6 +25,58 @@ const customFormat = format.combine(
   }),
 )
 
+function findRcFileInParentDirs(
+  fileName: string,
+  startDir: string = process.cwd(),
+): string | null {
+  let currentDir = path.resolve(startDir)
+
+  while (true) {
+    const filePath = path.join(currentDir, fileName)
+
+    // Check if the file exists in the current directory
+    if (fs.existsSync(filePath)) {
+      return filePath
+    }
+
+    const parentDir = path.dirname(currentDir)
+
+    if (parentDir === currentDir) {
+      return null
+    }
+
+    currentDir = parentDir
+  }
+}
+
+const configrc = findRcFileInParentDirs(".phapirc")
+
+let defaultLogLevel: LogLevel = LogLevel.INFO
+const definedLoggers: [string, LogLevel][] = []
+if (configrc) {
+  const config = JSON.parse(fs.readFileSync(configrc, "utf-8"))
+  const logConfig = config.log
+  // Set global default level if defined in config
+  if (
+    logConfig.defaultLevel &&
+    LogLevel[logConfig.defaultLevel as keyof typeof LogLevel] !== undefined
+  ) {
+    defaultLogLevel = LogLevel[logConfig.defaultLevel as keyof typeof LogLevel]
+  }
+  // Configure specific loggers
+  if (logConfig.loggers && typeof logConfig.loggers === "object") {
+    for (const loggerName in logConfig.loggers) {
+      const level = logConfig.loggers[loggerName]
+      if (LogLevel[level as keyof typeof LogLevel] !== undefined) {
+        definedLoggers.push([
+          loggerName,
+          LogLevel[level as keyof typeof LogLevel],
+        ])
+      }
+    }
+  }
+}
+
 const winstonLevels = {
   [LogLevel.ERROR]: 0,
   [LogLevel.WARN]: 1,
@@ -37,9 +89,9 @@ const winstonLevels = {
 }
 
 export class LoggerFactory {
-  private static defaultLogLevel: LogLevel = LogLevel.INFO
+  private static defaultLogLevel: LogLevel = defaultLogLevel
   private static loggers: Map<string, winston.Logger> = new Map()
-  private static localConfigLoaded = false
+  private static initialized = false
 
   static setDefaultLogLevel(level: LogLevel): void {
     this.defaultLogLevel = level
@@ -50,9 +102,36 @@ export class LoggerFactory {
   }
 
   public static getLogger(name?: string, level?: LogLevel): Logger {
-    if (!this.localConfigLoaded) {
-      this.loadConfigAndApply() // Load config only once
-      this.localConfigLoaded = true
+    if (!this.initialized) {
+      const defaultLogger = winston.createLogger({
+        levels: winstonLevels,
+        level: defaultLogLevel,
+        format: format.label({
+          label: "polkadot-hub-api",
+        }),
+        transports: [
+          new transports.Console({
+            format: customFormat,
+          }),
+        ],
+      })
+      this.loggers.set("default", defaultLogger)
+      for (const [loggerName, loggerLevel] of definedLoggers) {
+        const newLogger = winston.createLogger({
+          levels: winstonLevels,
+          level: loggerLevel,
+          format: format.label({
+            label: loggerName,
+          }),
+          transports: [
+            new transports.Console({
+              format: customFormat,
+            }),
+          ],
+        })
+        this.loggers.set(loggerName, newLogger)
+      }
+      this.initialized = true
     }
 
     if (!name) {
@@ -62,11 +141,11 @@ export class LoggerFactory {
     if (!this.loggers.has(name)) {
       const actualLevel = level || this.defaultLogLevel
       const newLogger = winston.createLogger({
-        levels: winstonLevels, // Use our custom levels mapping
+        levels: winstonLevels,
         level: actualLevel,
         format: format.label({
           label: name === "default" ? "polkadot-hub-api" : name,
-        }), // Add the logger name as a label
+        }),
         transports: [
           new transports.Console({
             format: customFormat,
@@ -83,82 +162,9 @@ export class LoggerFactory {
   }
 
   public static configureLogger(name: string, level: LogLevel): Logger {
-    const logger = LoggerFactory.getLogger(name) // Get existing or create new with current default
+    const logger = LoggerFactory.getLogger(name)
     logger.level = level
-    this.loggers.set(name, logger) // Update the logger in the map
+    this.loggers.set(name, logger)
     return logger
-  }
-
-  public static loadConfigAndApply(projectRoot: string = process.cwd()): void {
-    const rootSplit = projectRoot.split("/")
-    const root = rootSplit.lastIndexOf("packages")
-    let file = ".phapirc"
-    if (root >= 0) {
-      const diff = rootSplit.length - root
-      let navigation = ""
-      for (let i = 0; i < diff; i++) {
-        navigation += "../"
-      }
-      file = path.join(navigation, file)
-    }
-    const configFilePath = path.join(projectRoot, file)
-
-    try {
-      if (fs.existsSync(configFilePath)) {
-        const configFileContent = fs.readFileSync(configFilePath, "utf8")
-        const config = JSON.parse(configFileContent)
-
-        if (config.log) {
-          const logConfig = config.log
-
-          // Set global default level if defined in config
-          if (
-            logConfig.defaultLevel &&
-            LogLevel[logConfig.defaultLevel as keyof typeof LogLevel] !==
-              undefined
-          ) {
-            LoggerFactory.setDefaultLogLevel(
-              LogLevel[logConfig.defaultLevel as keyof typeof LogLevel],
-            )
-            console.log(
-              `[LoggerConfig] Default log level set to: ${logConfig.defaultLevel}`,
-            )
-          }
-
-          // Configure specific loggers
-          if (logConfig.loggers && typeof logConfig.loggers === "object") {
-            for (const loggerName in logConfig.loggers) {
-              if (
-                Object.prototype.hasOwnProperty.call(
-                  logConfig.loggers,
-                  loggerName,
-                )
-              ) {
-                const level = logConfig.loggers[loggerName]
-                if (LogLevel[level as keyof typeof LogLevel] !== undefined) {
-                  LoggerFactory.configureLogger(
-                    loggerName,
-                    LogLevel[level as keyof typeof LogLevel],
-                  )
-                } else {
-                  console.warn(
-                    `[LoggerConfig] Invalid log level "${level}" for logger "${loggerName}" in .phapirc. Skipping.`,
-                  )
-                }
-              }
-            }
-          }
-        }
-      } else {
-        console.info(
-          `[LoggerConfig] No .phapirc file found at ${configFilePath}. Using default logging levels.`,
-        )
-      }
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (error: any) {
-      console.error(
-        `[LoggerConfig] Error reading or parsing .phapirc file: ${error.message}`,
-      )
-    }
   }
 }
