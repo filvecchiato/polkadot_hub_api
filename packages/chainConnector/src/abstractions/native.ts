9ed74ab58e36223c037ab4ff75d301a516d31bf7
Filippo Vecchiato <filippo@parity.io>
2025-07-31 13:29:15 +0200

    extend native class

--- packages/chainConnector/src/abstractions/native.ts
+++ packages/chainConnector/src/abstractions/native.ts
@@ -14,4 +14,20 @@ export class NativeToken {
   get id(): string {
     throw new Error("Method 'id' not implemented.")
   }
+
+  get decimals(): number {
+    throw new Error("Method 'decimals' not implemented.")
+  }
+
+  get symbol(): string {
+    throw new Error("Method 'symbol' not implemented.")
+  }
+
+  get name(): string {
+    throw new Error("Method 'name' not implemented.")
+  }
+
+  get chainId(): string | undefined {
+    return this.originChainId
+  }
 }
